7fa581f2c31bb2fe3fe31654dd17468a870e0b3e
VishalKhannaS <vishalkhanna27062001@gmail.com>
2025-01-09 16:38:22 +0530

    new changes

---
+++ test.py
@@ -0,0 +1,26 @@
+print('hello world 1')
+"""
+This script prints 'hello world 1' and then generates a list of odd numbers from 1 to 100.
+Functions:
+    None
+Variables:
+    odd_numbers (list): A list to store odd numbers from 1 to 100.
+The script performs the following steps:
+1. Prints 'hello world 1'.
+2. Initializes an empty list called `odd_numbers`.
+3. Iterates through numbers from 1 to 100.
+4. Checks if the number is odd.
+5. If the number is odd, appends it to the `odd_numbers` list.
+6. Prints the list of odd numbers.
+"""
+# write a code to print 10 numbers
+
+def generate_odd_numbers():
+    odd_numbers = []
+    for i in range(1, 101, 2):
+        odd_numbers.append(i)
+    return odd_numbers
+
+odd_numbers = generate_odd_numbers()
+
+print(odd_numbers)
